ab34b73d12d76155a695c1fcfbd0279d61eb510c
gdu <gdu@TeamCity>
2022-07-08 11:43:11 +0000

    TeamCity change in 'Test / TestTeamCity' project: VCS roots of 'Build site' build configuration were updated

--- .teamcity/patches/buildTypes/BuildSite.kts
+++ .teamcity/patches/buildTypes/BuildSite.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("BuildSite")
     name = "Build site"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
 }))
 
