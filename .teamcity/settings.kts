22794cfed2f723341aadafb5f1c21d196fc4dc9c
Guillaume Dujardin <gdu@eonix.be>
2022-07-08 13:30:08 +0200

    Changed VCS trigger settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -30,7 +30,7 @@ version = "2022.04"
 
 project {
     buildType(BuildClassLib)
-    buildType(BuildSite)
+
 }
 
 object BuildClassLib : BuildType({
@@ -47,7 +47,7 @@ object BuildClassLib : BuildType({
 
     triggers {
         vcs {
-            branchFilter = "+:<default>"
+            enforceCleanCheckout = true
         }
     }
 
@@ -66,35 +66,4 @@ object BuildClassLib : BuildType({
     }
 })
 
-object BuildSite : BuildType({
-    name = "Build site"
-    publishArtifacts = PublishMode.SUCCESSFUL
-    artifactRules = "src/TestTeamCity.Site/bin/Release/net6.0"
 
-    var projectPath = "src/TestTeamCity.Site/TestTeamCity.Site.csproj"
-    var configuration = "Release"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    triggers {
-        vcs {
-            branchFilter = "+:<default>"
-        }
-    }
-
-    steps {
-        dotnetBuild {
-            name = "dotnet build"
-            projects = projectPath
-            configuration = configuration
-        }
-        dotnetPublish {
-            name = "dotnet publish"
-            projects = projectPath
-            configuration = configuration
-            args = "--no-build"
-        }
-    }
-})
